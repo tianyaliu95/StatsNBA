50f9e3fe53d7279d45ce0af40cdc3beae159fafc
Tianya Liu <tianyaliu1995@gmail.com>
2020-01-06 20:05:58 -0500

    Updates

---
+++ precache-manifest.e8af1b89ab18dec98d6767180bd786cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467cd9716ece2b2ba7ad9b4a246681f8",
+    "url": "/StatsNBA/index.html"
+  },
+  {
+    "revision": "46fa718d7052c49dd878",
+    "url": "/StatsNBA/static/css/2.0ddc80b1.chunk.css"
+  },
+  {
+    "revision": "eaef51d87ce366d8a619",
+    "url": "/StatsNBA/static/css/main.caa73319.chunk.css"
+  },
+  {
+    "revision": "46fa718d7052c49dd878",
+    "url": "/StatsNBA/static/js/2.4db6654e.chunk.js"
+  },
+  {
+    "revision": "34a79545e0edfd0e21d48a97e0251fd0",
+    "url": "/StatsNBA/static/js/2.4db6654e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eaef51d87ce366d8a619",
+    "url": "/StatsNBA/static/js/main.937f6360.chunk.js"
+  },
+  {
+    "revision": "00857e873a893c63c75d",
+    "url": "/StatsNBA/static/js/runtime-main.a4ae4d32.js"
+  },
+  {
+    "revision": "c4b7f268489da638b76561c4bf785858",
+    "url": "/StatsNBA/static/media/background.c4b7f268.jpg"
+  },
+  {
+    "revision": "9470e2d5c6c7f425edc8e9d8baebdb05",
+    "url": "/StatsNBA/static/media/nba-logoman-word-white.9470e2d5.svg"
+  }
+]);
